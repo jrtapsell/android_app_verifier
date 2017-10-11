6b14869fc3dafddcf89b61892475648908f21f79
James Tapsell <email@jrtapsell.co.uk>
2017-10-11 15:50:21 +0100

    Removed local repo

--- gitWrapper/src/test/kotlin/uk/co/jrtapsell/gitWrapper/RepoProvider.kt
+++ gitWrapper/src/test/kotlin/uk/co/jrtapsell/gitWrapper/RepoProvider.kt
@@ -7,8 +7,7 @@ data class Repo(val path: String, val state: SignatureStatus)
 
 /** Modify for each testing environment. */
 val TEST_REPOS = listOf(
-        Repo("./", SignatureStatus.GOOD),
-        Repo("/home/james/binBot", SignatureStatus.UNSIGNED)
+        Repo("./", SignatureStatus.GOOD)
 )
 
 class RepoProvider {
