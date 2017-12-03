e38ee239285400ae33f877110b5b748d683f3080
James Tapsell <email@jrtapsell.co.uk>
2017-12-03 17:02:44 +0000

    Documented isA

--- keybaseConnector/src/main/kotlin/uk/co/jrtapsell/fyp/keybaseConnector/extensions/Extensions.kt
+++ keybaseConnector/src/main/kotlin/uk/co/jrtapsell/fyp/keybaseConnector/extensions/Extensions.kt
@@ -2,4 +2,5 @@ package uk.co.jrtapsell.fyp.keybaseConnector.extensions
 
 import kotlin.reflect.KClass
 
+/** Runtime check if this class is a subclass of another class. */
 infix fun Class<*>.isA(other: KClass<*>) = other.java.isAssignableFrom(this)
