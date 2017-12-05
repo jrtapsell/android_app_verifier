acc3baed1f0e91b2827d0233d7313bc43b6ce306
James Tapsell <email@jrtapsell.co.uk>
2017-12-05 16:21:16 +0000

    Documented array apply

--- keybaseConnector/src/main/kotlin/uk/co/jrtapsell/fyp/keybaseConnector/extensions/Extensions.kt
+++ keybaseConnector/src/main/kotlin/uk/co/jrtapsell/fyp/keybaseConnector/extensions/Extensions.kt
@@ -6,5 +6,6 @@ import kotlin.reflect.KClass
 /** Runtime check if this class is a subclass of another class. */
 infix fun Class<*>.isA(other: KClass<*>) = other.java.isAssignableFrom(this)
 
+/** Applies a modification to each item in the array. */
 fun <T: Any> JSONArray.applyEach(block: JSONArray.(Int) -> T?) =
     (0 until length()).mapNotNull { block(it) }
